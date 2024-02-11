606ebd5e9749310cfc199f0cc7cc1c11f2bc0fae
leo <ljgulliver256@gmail.com>
2024-02-11 19:29:03 +0000

    working user loop

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,115 @@
-// #[derive(Debug)]
-// struct Noun {
-//     stem: String,
-//     number: Number,
-//     declension: Declension,
-//     gender: Gender,
-// }
-
-// impl Noun {
-//     fn new(stem: String, number: Number, declension: Declension, gender: Gender) -> Self {
-//         Noun {
-//             stem,
-//             number,
-//             declension,
-//             gender,
-//         }
-//     }
-
-//     fn format(self) {
-//         // todo
-//         return
-//     }
-// }
-
-// #[derive(Debug)]
-// enum Number {
-//     Singular,
-//     Plural,
-// }
-
-// #[derive(Debug)]
-// enum Declension {
-//     First,
-//     Second,
-//     Third,
-//     Fourth,
-//     Fifth,
-// }
-
-// #[derive(Debug)]
-// enum Gender {
-//     Masculine,
-//     Feminine,
-//     Neuter,
-// }
-
-//ignore nouns for now practice with verbs instead
-mod verb;
-
-// use std::net::SocketAddr;
-
-// use axum::response::Html;
-// use axum::Router;
-// use axum::routing::get;
-
-// #[tokio::main]
-fn main() {
-    // let routes_hello = Router::new().route(
-    //     "/hello",
-    //     get(|| async { Html("Hello World")})
-    // );
-
-    // let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
-    // println!("->> LISTENING on {:?}\n", addr);
-
-    // axum::Server::bind(&addr)
-    //     .serve(routes_hello.into_make_service())
-    //     .await
-    //     .unwrap();
+enum Person {
+    First,
+    Second,
+    Third,
+}
+enum Number {
+    Singular,
+    Plural,
+}
 
 
-    //let servus = Noun::new("serv".to_string(), Number::Singular, Declension::First, Gender::Masculine);
-    
-    verb::main();
+struct Verb {
+    stem: String,
+    translation: String,
+    person: Person,
+    number: Number,
+}
+
+impl Verb {
+    fn new(stem: String, translation: String, person: Person, number: Number) -> Self {
+        Verb {
+            stem,
+            translation,
+            person,
+            number,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Flaschard {
+    front: String,
+    back: String,
+}
+
+impl Flaschard {
+    fn new(front: String, back: String) -> Self {
+        Flaschard {
+            front,
+            back,
+        } 
+    }
+}
+
+fn fetch_latin(word: &Verb) -> String { // must borrow verb as do not want to give ownership to this function (otherwise verb will not be able to be used anywhere else)
+    //temporarily presume all verbs are active, indicative, present tense
+    match word.number {
+        Number::Singular => match word.person {
+            Person::First => word.stem.to_string() + "o",  //weird rust string stuff (see fetch_translation() for explanation)
+            Person::Second => word.stem.to_string() + "s",
+            Person::Third => word.stem.to_string() + "t",
+        },
+        Number::Plural => match word.person {
+            Person::First => word.stem.to_string() + "mus",
+            Person::Second => word.stem.to_string() + "tis",
+            Person::Third => word.stem.to_string() + "nt",
+        },
+    }
+}
+
+fn fetch_translation(word: &Verb) -> String {
+    match word.number {
+        Number::Singular => match word.person {
+            Person::First => "i ".to_string() + &word.translation, //weird rust string stuff. to use "+" first input must be String and second input must be &str
+            Person::Second => "you ".to_string() + &word.translation,
+            Person::Third => "he ".to_string() + &word.translation, 
+        },
+        Number::Plural => match word.person {
+            Person::First => "we ".to_string() + &word.translation,
+            Person::Second => "you (pl) ".to_string() + &word.translation,
+            Person::Third => "they ".to_string() + &word.translation,
+        },
+    }
+}
+
+use std::io;
+
+
+pub fn main() {
+    let mut exampleword: Verb = Verb::new("mone".to_string(), "warn".to_string(), Person::First, Number::Singular);
+    let card1: Flaschard = Flaschard::new(fetch_latin(&exampleword), fetch_translation(&exampleword));
+    exampleword.number = Number::Plural;
+    let card2: Flaschard = Flaschard::new(fetch_latin(&exampleword), fetch_translation(&exampleword));
+    exampleword.person = Person::Third;
+    let card3: Flaschard = Flaschard::new(fetch_latin(&exampleword), fetch_translation(&exampleword));
+
+    let list: Vec<Flaschard> = vec![card1, card2, card3];
+
+    for item in list {
+        let mut input = String::new();
+
+        println!("{}", item.front);
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => (),
+            Err(error) => eprintln!("error reading input: {}", error)
+        }
+
+        if input.trim() == item.back {
+            println!("correct");
+        }   
+    }
+
+    // let mut input = String::new();
+    // println!("{}", card1.front);
+    // match io::stdin().read_line(&mut input) {
+    //     Ok(_) => (),
+    //     Err(error) => eprintln!("error reading input: {}", error)
+    // }
+
+    // if input.trim() == card1.back {
+    //     println!("correct");
+    // }
+
+
 }
