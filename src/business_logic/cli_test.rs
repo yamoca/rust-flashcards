f94b4cbb3d524301485346153cb6da9cc6e35860
leo <ljgulliver256@gmail.com>
2024-02-24 15:15:03 +0000

    fully cleaned project

---
+++ src/business_logic/cli_test.rs
@@ -0,0 +1,67 @@
+use std::io;
+
+// mod business_logic;
+
+fn main() { 
+    runflashcards();
+}
+
+fn runflashcards() {
+    let words = vec![
+        Verb::new(vec!["porto".to_string(), "portare".to_string(), "portavi".to_string(), "portatus".to_string()], "carry".to_string(), Tense::Perfect, Conjugation::First),
+        Verb::new(vec!["moneo".to_string(), "monere".to_string(), "monui".to_string(), "monitus".to_string()], "warn".to_string(), Tense::Perfect, Conjugation::Second),
+        Verb::new(vec!["traho".to_string(), "trahere".to_string(), "traxi".to_string(), "tractus".to_string()], "drag".to_string(), Tense::Perfect, Conjugation::Third),
+        Verb::new(vec!["audio".to_string(), "audire".to_string(), "audivi".to_string(), "auditus".to_string()], "hear".to_string(), Tense::Perfect, Conjugation::Fourth),
+    ];
+    userloop(words);
+}
+
+fn userloop(words: Vec<Verb>) {
+    let mut deck: Vec<Flaschard> = Vec::new();
+
+    for word in words {
+        match fetch_translation(&word) {
+            Ok(translation) => {
+                deck.push(Flaschard::new(create_root(&word), translation))
+            }
+            Err(()) => {
+                eprintln!("failed to fetch translation for {:?}", word)
+            }
+        }
+    }
+    
+
+    for card in deck {
+        let mut input = String::new();
+
+        println!("{}", card.front);
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => (),
+            Err(error) => eprintln!("error reading input: {}", error)
+        }
+
+        if input.trim() == card.back {
+            println!("correct");
+        } else {
+            println!("incorrect");
+            println!("correct answer: {}", card.back);
+        } 
+    }
+
+    // let mut input = String::new();
+    // println!("{}", card0.front);
+    // match io::stdin().read_line(&mut input) {
+    //     Ok(_) => (),
+    //     Err(error) => eprintln!("error reading input: {}", error)
+    // }
+
+    // if input.trim() == card0.back {
+    //     println!("correct");
+    // }
+
+
+}
+
+// today: get better way to manage and create words and flashcards and card stack (card stack is name of vector)
+//    smash out tenses     
+// for "he" (2rd person singular), translation is incorrect - must add -s to words e.g he warns
--- src/business_logic/utils.rs
+++ src/business_logic/utils.rs
@@ -1,6 +1,5 @@
 use rusqlite::{Connection, Result};
-use crate::data::*;
-
+use super::data::*;
 
 // query example
 // pub fn main() {
@@ -13,101 +12,7 @@ use crate::data::*;
 // }
 
 
-pub fn get_conjugated_english(tense: &Tense, word: &str) -> Result<String> {
-    let conn = Connection::open("output_database.sqlite")?;
-
-    let columnname = match tense {
-        Tense::Present => "present_tense",
-        Tense::Perfect => "perfect_tense",
-        Tense::Imperfect => "imperfect_tense",
-    };
-
-    let sqlquery = format!("SELECT {} FROM data WHERE present_tense=?1", columnname);
-    let mut stmt = conn.prepare(&sqlquery)?;
-
-    let res: String = stmt.query_row([&word], |row| {
-        row.get(0)
-    })?;
-
-    Ok(res)
-}
-
-
-
-
-pub fn create_root(word: &Verb) -> String { // must borrow verb as do not want to give ownership to this function (otherwise verb will not be able to be used anywhere else)
-    // step one: create root word e.g port goes to porta (in most cases)
-    let mut principle_parts = word.principle_parts.clone();
-    let mut stem = String::new();
-
-    // come back to. diff forms have diff stems (perfect vs others - see principle parts)
-
-
-
-
-    match word.tense {
-        Tense::Present => {
-            principle_parts[0].pop(); // removes last letter from first principle part e.g porto -> port, moneo -> mone
-            stem = principle_parts[0].clone();
-
-            match word.conjugation {
-                Conjugation::First => {
-                    if let (Number::Singular, Person::First) = (&word.number, &word.person) {
-                        // No modification needed for unchanged case
-                    } else {
-                        stem.push('a');
-                    }
-                } 
-                Conjugation::Second => stem.push('e'),
-                Conjugation::Third => match (&word.number, &word.person) {
-                    (Number::Singular, Person::First) => stem.push('h'),
-                    (Number::Plural, Person::Third) => stem.push_str("hu"),
-                    _ => stem.push_str("hi"),
-                },
-                Conjugation::Fourth => match (&word.number, &word.person) {
-                    (Number::Plural, Person::Third) => stem.push_str("iu"),
-                    _ => stem.push('i'),
-                }
-                Conjugation::Fifth => todo!(),
-            };
-
-        },
-        Tense::Imperfect => {
-            // imperfect is fine to scipt cause its regular like first principle part
-            principle_parts[0].pop(); // removes last letter from first principle part e.g porto -> port, moneo -> mone
-            stem = principle_parts[0].clone(); // imperfect uses the first principle part
-
-            match word.conjugation {
-                Conjugation::First => stem.push('a'),
-                Conjugation::Second => stem.push('e'),
-                Conjugation::Third => stem.push_str("he"),
-                Conjugation::Fourth => stem.push_str("ie"),
-                Conjugation::Fifth => todo!(),
-            };
-        },
-        Tense::Perfect => {
-            principle_parts[2].pop(); // perfect is 3rd principle part
-            stem = principle_parts[2].clone(); // just remove last letter - portavi -> portav, traxi -> trax
-
-            // match word.conjugation {
-            //     Conjugation::First => stem.push_str("av"),
-            //     Conjugation::Second => stem.push('u'),
-            //     Conjugation::Third => stem.push('x'),
-            //     Conjugation::Fourth => {
-            //         if let (Number::Singular, Person::First) = (&word.number, &word.person) {
-            //             stem.push('v')
-            //         } else {
-            //             stem.push_str("iv")
-            //         }
-            //     } 
-            //     Conjugation::Fifth => todo!(),
-            // }
-        },
-    };
-    // next step
-    apply_tense(word, stem)
-}
-
+// latin
 pub fn apply_tense(word: &Verb, mut res: String) -> String {
     match word.tense {
         Tense::Present => match word.number {
@@ -208,3 +113,104 @@ pub fn fetch_translation(word: &Verb) -> Result<String, ()> {
     Ok(res)
     
 }
+
+
+
+// helper functions
+
+fn get_conjugated_english(tense: &Tense, word: &str) -> Result<String> {
+    let conn = Connection::open("output_database.sqlite")?;
+
+    let columnname = match tense {
+        Tense::Present => "present_tense",
+        Tense::Perfect => "perfect_tense",
+        Tense::Imperfect => "imperfect_tense",
+    };
+
+    let sqlquery = format!("SELECT {} FROM data WHERE present_tense=?1", columnname);
+    let mut stmt = conn.prepare(&sqlquery)?;
+
+    let res: String = stmt.query_row([&word], |row| {
+        row.get(0)
+    })?;
+
+    Ok(res)
+}
+
+
+
+
+fn create_root(word: &Verb) -> String { // must borrow verb as do not want to give ownership to this function (otherwise verb will not be able to be used anywhere else)
+    // step one: create root word e.g port goes to porta (in most cases)
+    let mut principle_parts = word.principle_parts.clone();
+    let mut stem = String::new();
+
+    // come back to. diff forms have diff stems (perfect vs others - see principle parts)
+
+
+
+
+    match word.tense {
+        Tense::Present => {
+            principle_parts[0].pop(); // removes last letter from first principle part e.g porto -> port, moneo -> mone
+            stem = principle_parts[0].clone();
+
+            match word.conjugation {
+                Conjugation::First => {
+                    if let (Number::Singular, Person::First) = (&word.number, &word.person) {
+                        // No modification needed for unchanged case
+                    } else {
+                        stem.push('a');
+                    }
+                } 
+                Conjugation::Second => stem.push('e'),
+                Conjugation::Third => match (&word.number, &word.person) {
+                    (Number::Singular, Person::First) => stem.push('h'),
+                    (Number::Plural, Person::Third) => stem.push_str("hu"),
+                    _ => stem.push_str("hi"),
+                },
+                Conjugation::Fourth => match (&word.number, &word.person) {
+                    (Number::Plural, Person::Third) => stem.push_str("iu"),
+                    _ => stem.push('i'),
+                }
+                Conjugation::Fifth => todo!(),
+            };
+
+        },
+        Tense::Imperfect => {
+            // imperfect is fine to scipt cause its regular like first principle part
+            principle_parts[0].pop(); // removes last letter from first principle part e.g porto -> port, moneo -> mone
+            stem = principle_parts[0].clone(); // imperfect uses the first principle part
+
+            match word.conjugation {
+                Conjugation::First => stem.push('a'),
+                Conjugation::Second => stem.push('e'),
+                Conjugation::Third => stem.push_str("he"),
+                Conjugation::Fourth => stem.push_str("ie"),
+                Conjugation::Fifth => todo!(),
+            };
+        },
+        Tense::Perfect => {
+            principle_parts[2].pop(); // perfect is 3rd principle part
+            stem = principle_parts[2].clone(); // just remove last letter - portavi -> portav, traxi -> trax
+
+            // match word.conjugation {
+            //     Conjugation::First => stem.push_str("av"),
+            //     Conjugation::Second => stem.push('u'),
+            //     Conjugation::Third => stem.push('x'),
+            //     Conjugation::Fourth => {
+            //         if let (Number::Singular, Person::First) = (&word.number, &word.person) {
+            //             stem.push('v')
+            //         } else {
+            //             stem.push_str("iv")
+            //         }
+            //     } 
+            //     Conjugation::Fifth => todo!(),
+            // }
+        },
+    };
+    // next step
+    apply_tense(word, stem)
+}
+
+
